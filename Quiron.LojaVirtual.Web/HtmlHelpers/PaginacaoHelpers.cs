7645026043e4337053ba00c26496af675e3b30ed
claudioht <claudioht@gmail.com>
2017-04-05 11:16:45 -0300

    Teste1

--- Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -1,9 +1,6 @@
 ﻿using Quiron.LojaVirtual.Web.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Web.HtmlHelpers
@@ -14,13 +11,13 @@ namespace Quiron.LojaVirtual.Web.HtmlHelpers
         {
             StringBuilder resultado = new StringBuilder();
 
-            for (int i = 1; i < paginacao.TotalPagina; i++)
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", paginaUrl(i));
                 tag.InnerHtml = i.ToString();
 
-                if (i == paginacao.PaginaAtual) { }
+                if (i == paginacao.PaginaAtual) 
                 {
                     tag.AddCssClass("selected");
                     tag.AddCssClass("btn-primary");
--- Quiron.LolaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LolaVirtual.UnitTest/UnitTest1.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
 
 namespace Quiron.LolaVirtual.UnitTest
 {
     [TestClass]
     public class UnitTestQuiron
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void pagina()
         {
+            HtmlHelper html = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+               + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString());
         }
     }
 }
