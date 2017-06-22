c4135b69f532c9a8f9555196adbf20143e5e22bb
claudioht <claudioht@gmail.com>
2017-06-22 17:58:15 -0300

    Administrador Login

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Dominio.Entidade
 {
     public class Produto
     {
+        [HiddenInput(DisplayValue= false)]
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage ="Digite o nome do Produto")]
         public string Nome { get; set; }
+
+        [DataType(DataType.MultilineText)]
+
+        [Required(ErrorMessage ="Digite a Descrição")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage ="Digite o Valor")]
+        [Range(0.01,double.MaxValue,ErrorMessage ="Valor Inválido")]
         public decimal Preco { get; set; }
 
+        [Required(ErrorMessage ="Digite a Categoria")]
         public string Categoria { get; set; }
  
     }
